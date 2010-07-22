cd36de334c7de05c223318161ec60099f46edff2
fatherstorm@gmail.com <fatherstorm@gmail.com@0ee3e418-066a-3e69-814a-1e9009514b71>
2010-07-22 16:44:32 +0000

    Initial import

---
+++ config/ProjectConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'C:\\Program Files\\WaterProof\\PHPEdit\\3.6.0\\Extensions\\Symfony\\distribution\\1.4\\lib/autoload/sfCoreAutoload.class.php';
+sfCoreAutoload::register();
+
+class ProjectConfiguration extends sfProjectConfiguration
+{
+  public function setup()
+  {
+    $this->enablePlugins('sfDoctrinePlugin');
+  }
+}
